89c5c551ba112515a235f23559ff05835134288e
thejazzyone <48731014+thejazzyone@users.noreply.github.com>
2019-04-15 11:38:35 -0400

    Update modifiedpHonestyAdjust.m

--- modifiedpHonestyAdjust.m
+++ modifiedpHonestyAdjust.m
@@ -4,21 +4,22 @@ for i = 2:6
     condition = switchArray(i);
     switch condition
         case 1 % they lied
-            liesN = lies(playerNumber);
+        
+            liesN = lies(i);
             weight2 = (1/(1+1*liesN))*10;
             weight1 = 10-weight2;
-            honestValue = pHonestArray(playerNumber);
+            honestValue = pHonestArray(i);
             x = honestValue;
             newHonestValue = (weight1*x+weight2*(x/2))/10;
-            pHonestArray(playerNumber) = newHonestValue;
+            pHonestArray(i) = newHonestValue;
         case 2 % they told the truth
-            truthsN = truths(playerNumber);
-            honestValue = pHonestArray(playerNumber);
+            truthsN = truths(i);
+            honestValue = pHonestArray(i);
             weight2 = (1/(1+0.1*truthsN))*10;
             weight1 = 10-weight2;
             x = honestValue;
             newHonestValue = (weight1*x+weight2*((1-x)/2+x))/10;
-            pHonestArray(playerNumber) = newHonestValue;
+            pHonestArray(i) = newHonestValue;
         case 3
             % there is no change to pHonestArray for this player
     end
