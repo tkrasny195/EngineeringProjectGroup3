c4eb8b8a9deab3a6bb81c9c352c228f1a65868ad
thejazzyone <48731014+thejazzyone@users.noreply.github.com>
2019-04-16 15:01:01 -0400

    Update changeLikelyBS.m

--- changeLikelyBS.m
+++ changeLikelyBS.m
@@ -3,13 +3,10 @@ function likelyBS = changeLikelyBS(bs,rounds,turn,nsDice,bets,totalTurns,nDice,l
 % Thomas Kasl
 
 turnOfInterest = reduceTurnNumber(turn,nsDice);
-
-if bs
-    if rounds == 0
-        matrix1 = cell2mat(bets);
-        likelyBS(turnOfInterest) = binocdf((matrix1(1,(totalTurns-1))),nDice,(1/3));
-    else
-        matrix1 = cell2mat(bets);
-        likelyBS(turnOfInterest) = (binocdf((matrix1(1,(totalTurns-1))),nDice,(1/3))+likelyBS(turnOfInterest))/2;
-    end
+if rounds == 0
+    matrix1 = cell2mat(bets);
+    likelyBS(turnOfInterest) = binocdf((matrix1(1,(totalTurns-1))),nDice,(1/3));
+else
+    matrix1 = cell2mat(bets);
+    likelyBS(turnOfInterest) = (binocdf((matrix1(1,(totalTurns-1))),nDice,(1/3))+likelyBS(turnOfInterest))/2;
 end
