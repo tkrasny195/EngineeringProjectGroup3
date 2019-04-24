e4d13953f145adcebe6d04a9fa2f98196aff0da1
thejazzyone <48731014+thejazzyone@users.noreply.github.com>
2019-04-23 22:17:25 -0400

    Delete gamesCalc.m

--- gamesCalc.m
+++
@@ -1,8 +0,0 @@
-function games = gamesCalc(level,games,nGamesInFirstLevel,nGamesInHigherLevels)
-% 4/11/19
-% Thomas Kasl
-if level == 1
-    games = nGamesInFirstLevel;
-else
-    games = nGamesInFirstLevel*games+nGamesInHigherLevels;
-end
